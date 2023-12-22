d9ac92d36cd3f1daf9c9b96e4d22f1a7946fa683
Kol-Kol-Kol <kol-7787@mail.ru>
2023-12-22 15:56:36 +0300

    Task_1_2_4

---
+++ src/kata.shcherbina.j_core.first_block/Task_1_2_4.java
@@ -0,0 +1,14 @@
+package kata.shcherbina.j_core.first_block;
+
+public class Task_1_2_4 {
+    public static void main(String[] args) {
+        byte varbyte = 10;
+        short varshort = -120;
+        int varint = 100_100_500;
+        long varlong = 10_100_100_100L;
+        float varfloat = 3.5f;
+        double vardouble = 12.12341;
+        char varchar = '!';
+        boolean varboolean = false;
+    }
+}
